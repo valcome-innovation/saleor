82a694b40521a67f621f99d82bc1ba281dcac2a6
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2020-06-24 17:03:40 +0200

    Added a better name for the docker stack

--- restart-test-api.sh
+++ restart-test-api.sh
@@ -1,6 +1,6 @@
-docker-compose -f docker-compose.test.yml down
-docker-compose -f docker-compose.test.yml build
-docker-compose -f docker-compose.test.yml run --rm api python3 manage.py migrate
-docker-compose -f docker-compose.test.yml run --rm api python3 manage.py collectstatic --noinput
-docker-compose -f docker-compose.test.yml run -u root --rm api python3 manage.py populatedb --createsuperuser
-docker-compose -f docker-compose.test.yml up -d
+docker-compose -f docker-compose.test.yml -p saleor-test down
+docker-compose -f docker-compose.test.yml -p saleor-test build
+docker-compose -f docker-compose.test.yml -p saleor-test run --rm api python3 manage.py migrate
+docker-compose -f docker-compose.test.yml -p saleor-test run --rm api python3 manage.py collectstatic --noinput
+docker-compose -f docker-compose.test.yml -p saleor-test run -u root --rm api python3 manage.py populatedb --createsuperuser
+docker-compose -f docker-compose.test.yml -p saleor-test up -d
