90fbdc55e261a571e417a6cdfc71d1d3c79db2bc
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2020-07-30 12:13:34 +0200

    Add metadata to stream ticket on checkout complete

--- saleor/graphql/checkout/mutations.py
+++ saleor/graphql/checkout/mutations.py
@@ -808,9 +808,13 @@ class CheckoutComplete(BaseMutation):
                     {"redirect_url": error}, code=AccountErrorCode.INVALID
                 )
 
+        checkout.get_value_from_metadata('STREAM_ID')
+
         # Add Stream ticket
         stream_ticket = StreamTicket()
-        stream_ticket.stream_id = "Test"
+        stream_ticket.stream_id = checkout.get_value_from_metadata('STREAM_ID')
+        stream_ticket.league_id = checkout.get_value_from_metadata('LEAGUE_ID')
+        stream_ticket.team_id = checkout.get_value_from_metadata('TEAM_ID')
         stream_ticket.type = "single"
         stream_ticket.save()
         user.stream_tickets.add(stream_ticket)
