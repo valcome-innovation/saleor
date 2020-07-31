546c8ae820b407f45b4cf9fe32e475e5f677762c
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2020-07-31 13:53:59 +0200

    Refactored docker build to user entrypoint.sh file

---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -v MIGRATE ]; then
+  echo "Applying database migrations..."
+  python3 manage.py migrate
+fi
+
+if [ -v COLLECT_STATIC ]; then
+  echo "Collecting static files..."
+  python3 manage.py collectstatic --noinput
+fi
+
+if [ -v POPULATE ]; then
+  echo "Populating database with dummy data..."
+  python3 manage.py populatedb --createsuperuser
+fi
+
+if [ -v STREAM_SETUP ]; then
+  echo "Populating database with dummy data..."
+  python3 manage.py streamsetup
+fi
+
+if [ -v PROD_MODE ]; then
+  echo "Running saleor API in production mode..."
+  uwsgi --ini /app/saleor/wsgi/uwsgi.ini
+else
+  echo "Running saleor API in development mode..."
+  python manage.py runserver 0.0.0.0:8000
+fi
--- restart-test-api.sh
+++
@@ -1,6 +0,0 @@
-docker-compose -f docker-compose.test.yml -p saleor-test down
-docker-compose -f docker-compose.test.yml -p saleor-test build
-docker-compose -f docker-compose.test.yml -p saleor-test run --rm api python3 manage.py migrate
-docker-compose -f docker-compose.test.yml -p saleor-test run --rm api python3 manage.py collectstatic --noinput
-docker-compose -f docker-compose.test.yml -p saleor-test run -u root --rm api python3 manage.py populatedb --createsuperuser
-docker-compose -f docker-compose.test.yml -p saleor-test up -d
