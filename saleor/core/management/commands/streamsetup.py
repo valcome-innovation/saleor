a71720a4facf6d092a04a9ecbc5aaf5f492013d0
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-11-09 12:56:00 +0100

    Added bulk stream

--- saleor/core/management/commands/streamsetup.py
+++ saleor/core/management/commands/streamsetup.py
@@ -5,12 +5,14 @@ from django.core.management import call_command
 from django.core.management.base import BaseCommand
 from django.db import connection
 
+from ....account.models import StreamTicket, User
 from ....account.utils import create_superuser
 from ....site.models import AuthenticationBackends
 from ...utils.random_data import (
     add_address_to_admin,
     create_auth_key,
     create_page,
+    create_users,
     create_app_with_token,
     create_products_by_schema
 )
@@ -22,6 +24,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         credentials = {"email": "dev@valcome.tv", "password": "m5$ktVp$3cRv%8DtvR"}
         create_superuser(credentials)
+
+        user = User.objects.filter(email="dev@valcome.tv").first()
+        user.stream_tickets.add(self.create_stream_ticket())
+        user.save()
+
         add_address_to_admin(credentials["email"])
         self.create_info_pages()
         self.create_auth_keys()
@@ -53,3 +60,13 @@ class Command(BaseCommand):
             self.stdout.write(msg)
         for msg in create_page("AGB", "terms"):
             self.stdout.write(msg)
+
+    def create_stream_ticket(self):
+        stream_ticket = StreamTicket()
+        stream_ticket.game_id = "14"
+        stream_ticket.season_id = "3"
+        stream_ticket.team_id = "6"
+        stream_ticket.expires = None
+        stream_ticket.type = "single"
+        stream_ticket.save()
+        return stream_ticket
