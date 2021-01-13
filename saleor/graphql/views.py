9134e9d576c7b7f1a540d11d2b1db74864564574
Simon Kepplinger <simon.kepplinger@valcome.at>
2021-01-13 10:21:39 +0100

    Debug

--- saleor/graphql/views.py
+++ saleor/graphql/views.py
@@ -247,6 +247,7 @@ class GraphQLView(View):
         if content_type == "application/graphql":
             return {"query": request.body.decode("utf-8")}
         if content_type == "application/json":
+            print(request.body)
             body = request.body.decode("utf-8")
             return json.loads(body)
         if content_type in ["application/x-www-form-urlencoded", "multipart/form-data"]:
