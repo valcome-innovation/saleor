b78762d712388dd9900e67ad1e48c59b9fe78122
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2021-02-16 12:54:14 +0100

    Reverse app id check

--- saleor/stripe_webhooks/views.py
+++ saleor/stripe_webhooks/views.py
@@ -30,12 +30,12 @@ def handle_stripe_webhook_event(request, event_type, payment_intent):
     if has_correct_app_id(payment_intent) and has_sofort_payment_method(payment_intent):
         return process_sofort_webhook_event(request, event_type, payment_intent)
     else:
-        return HttpResponse(status=200)
+        return HttpResponse(status=200) # skip webhooks not meant for this app
 
 
 def has_correct_app_id(payment_intent):
-    return not hasattr(payment_intent.metadata, 'appId') \
-           or payment_intent.metadata.appId == settings.APP_ID
+    return hasattr(payment_intent.metadata, "appId") \
+           and payment_intent.metadata.appId == settings.APP_ID
 
 
 def has_sofort_payment_method(payment_intent):
@@ -44,11 +44,11 @@ def has_sofort_payment_method(payment_intent):
 
 
 def process_sofort_webhook_event(request, event_type, payment_intent):
-    if event_type == 'payment_intent.processing':
+    if event_type == "payment_intent.processing":
         info = Info(request)
         handle_sofort(payment_intent, info)
         return HttpResponse(status=200)
-    elif event_type == 'payment_intent.failed':
+    elif event_type == "payment_intent.failed":
         return HttpResponse(status=400)
     else:
         return HttpResponse(status=200)
