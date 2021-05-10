5e5ebc1e9e4a1e85d0ec70f678cfe5c7e502799d
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2021-05-10 10:16:17 +0200

    Add local startup scripts

---
+++ development/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd $(dirname "$0")
+
+docker network create --driver bridge live-local-backend || true
+
+docker-compose -p live-local down || true
+docker-compose -p live-local build
+docker-compose -p live-local up
