d1fc49928606845083ddc7c625d503df5e702d2f
Simon Kepplinger <simon.kepplinger@valcome.at>
2021-06-09 16:11:55 +0200

    Added basic fields for new ticket

--- saleor/streaming/models.py
+++ saleor/streaming/models.py
@@ -5,15 +5,13 @@ from saleor.account.models import User
 
 class TicketType:
     SINGLE = "single"
-    DAY = "day"
-    TEAM = "team"
-    LEAGUE = "league"
+    TIMED = "timed"
+    SEASON = "season"
 
     CHOICES = [
         (SINGLE, 'single'),
-        (DAY, 'day'),
-        (TEAM, 'team'),
-        (LEAGUE, 'league')
+        (TIMED, 'timed'),
+        (SEASON, 'season'),
     ]
 
 
@@ -27,11 +25,16 @@ class StreamTicket(models.Model):
         null=True,
         on_delete=models.SET_NULL
     )
+    access_code = models.CharField(max_length=256, blank=True, null=True)
     version = models.IntegerField(default=1)
     game_id = models.CharField(max_length=256, blank=True, null=True)
-    team_id = models.CharField(max_length=256, blank=True, null=True)
     season_id = models.CharField(max_length=256, blank=True, null=True)
+    start_time = models.DateTimeField(default=None, editable=True, blank=True, null=True)
     expires = models.DateTimeField(default=None, editable=True, blank=True, null=True)
+    league_ids = models.CharField(max_length=256, blank=True, null=True)
+    team_ids = models.CharField(max_length=256, blank=True, null=True)
+    is_free = models.BooleanField(default=False, null=False)
+    timed_type = models.CharField(max_length=256, default="none", null=False)
 
     class Meta:
         ordering = ("expires",)
--- saleor/streaming/stream_ticket.py
+++ saleor/streaming/stream_ticket.py
@@ -11,29 +11,28 @@ from ..order.models import Order
 
 
 def create_stream_ticket_from_order(order: "Order"):
-    (game_id, season_id, team_id) = get_stream_meta(order)
-    expires = None  # TODO: set expires for day ticket
+    (game_id, season_id, expires, start_time, team_ids, league_ids) = get_stream_meta(order)
 
     stream_ticket = StreamTicket()
     stream_ticket.user = order.user
     stream_ticket.game_id = game_id or None
     stream_ticket.season_id = season_id or None
-    stream_ticket.team_id = team_id or None
-    stream_ticket.expires = expires
-    stream_ticket.type = determine_ticket_type(game_id, season_id, team_id, expires)
+    stream_ticket.start_time = start_time or None
+    stream_ticket.expires = expires or None
+    stream_ticket.league_ids = league_ids or None
+    stream_ticket.team_ids = team_ids or None
+    stream_ticket.type = determine_ticket_type(game_id, season_id, team_ids, expires)
 
     stream_ticket.save()
 
 
-def determine_ticket_type(game_id, season_id, team_id, expires):
-    if game_id is not None and season_id is None and team_id is None:
+def determine_ticket_type(game_id, season_id, expires):
+    if game_id is not None and season_id is None and expires is None:
         return 'single'
-    elif season_id is not None and team_id is not None and game_id is None:
-        return 'team'
-    elif season_id is not None and team_id is None and game_id is None:
-        return 'league'
-    elif expires is not None and season_id is None and team_id is None and game_id is None:
-        return 'day'
+    elif season_id is not None and expires is None and game_id is None:
+        return 'season'
+    elif expires is not None and season_id is None and game_id is None:
+        return 'timed'
     else:
         raise ValidationError(
             "Could not determine TicketType from input parameters",
@@ -42,8 +41,9 @@ def determine_ticket_type(game_id, season_id, team_id, expires):
 
 
 def validate_stream_checkout_with_product(checkout: "Checkout", lines: "list"):
-    (game_id, season_id, team_id) = get_stream_meta(checkout)
-    stream_ticket_type = determine_ticket_type(game_id, season_id, team_id, None)
+    (game_id, season_id, expires, start_time, team_ids, league_ids) = get_stream_meta(checkout)
+
+    stream_ticket_type = determine_ticket_type(game_id, season_id, expires)
 
     if len(lines) == 1 and lines[0].variant.product.attributes.count() >= 1:
         attributes = lines[0].variant.product.attributes.all()
@@ -64,7 +64,10 @@ def validate_stream_checkout_with_product(checkout: "Checkout", lines: "list"):
 def get_stream_meta(meta_object: "ModelWithMetadata"):
     return from_meta('GAME_ID', meta_object), \
            from_meta('SEASON_ID', meta_object), \
-           from_meta('TEAM_ID', meta_object)
+           from_meta('EXPIRE', meta_object), \
+           from_meta('START_TIME', meta_object), \
+           from_meta('TEAM_IDS', meta_object), \
+           from_meta('LEAGUE_IDS', meta_object)
 
 
 def from_meta(key, meta_object: "ModelWithMetadata"):
